d96489e5ccd2fb7f8f4f93f7259f846dbacd4327
Graeme Smith <30961431+smithgraeme@users.noreply.github.com>
2019-06-23 15:33:07 -0600

    Update deployment strategy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,4 @@ set -e
 
 # run from the parent of the scripts directory (one level up)
 echo "Deploying to stage ${stage}"
-
-terraform init
-terraform workspace select $stage || terraform workspace new $stage
-terraform apply -var db_password="${dbPassword}" -auto-approve
-
 sls deploy --host "$(terraform output dbAddress)"
--- scripts/undeploy.sh
+++
@@ -1,2 +0,0 @@
-terraform destroy -auto-approve -var db_password="_" #PW doesn't matter for destroy
-sls remove
